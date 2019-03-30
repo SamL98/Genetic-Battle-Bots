5ca0b1237711e3eba4934f34ac2c9c828bceb1fb
Sam Lerner <lerner98@gmail.com>
2019-03-30 14:58:13 -0400

    git move filenames

--- render.py
+++ render.py
@@ -6,7 +6,7 @@ from agents.agent import AgentType
 from wall import Wall
 
 BLACK = (0, 0, 0)
-LW = 5
+LW = 1
 
 def get_fov_line_params(x, y, theta):
 	m = np.arctan(theta)
@@ -17,28 +17,30 @@ def render_circ(agent, canvas):
 	factor = 10
 	w = agent.world_dims[1] * factor
 
-	if agent.theta < 3*np.pi/2 and agent.theta >= np.pi/2:
+	theta = -agent.theta*np.pi/180
+	fov = agent.fov*np.pi/180
+
+	if -theta < 3*np.pi/2 and -theta >= np.pi/2:
 		w *= -1
 
 	circ = agent.circ
 	x, y = circ.x, circ.y
 
-	m1, b1 = get_fov_line_params(x, y, agent.theta-agent.fov/2)
-	m2, b2 = get_fov_line_params(x, y, agent.theta+agent.fov/2)
+	m1, b1 = get_fov_line_params(x, y, theta-fov/2)
+	m2, b2 = get_fov_line_params(x, y, theta+fov/2)
 
 	pts = np.array([
 		[x, y],
 		[w, m1*w + b1],
 		[w, m2*w + b2]
 	], dtype=np.int32)
-	print(pts)
-	pts = pts.reshape((-1, 1, 2))
 
-	cv.polylines(canvas, pts, True, (0, 0, 255), LW)
+	cv.polylines(canvas, [pts], True, (0, 0, 0), LW)
 
 	cv.circle(canvas, (x, y), circ.r, BLACK, -1)
 
 	if agent.agent_type == AgentType.Melee and agent.melee_active:
 		rad = agent.r + agent.m_r
-		start = agent.theta - agent.m_theta/2 
+		m_theta = agent.m_theta*np.pi/180
+		start = agent.theta - m_theta/2 
 		cv.ellipse(canvas, (x, y), (rad, rad), 0, start, agent.m_theta, BLACK, LW)
