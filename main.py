701acf63d56719fcf14047dde2c3c419f1a6020e
John Honaker <honaker215@gmail.com>
2019-03-30 15:07:36 -0400

    Added binding of network to game.

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-import agent as ag
+import agents.agent as ag
+from agents.range_agent import RangeAgent
 import nn
 import genetic as gen
+
+
+def orthonormal_vector(theta):
+    tant = np.tan(theta)
+    c = - np.sqrt(1 / ( tant ** 2 + 1))
+    u = -c * tant
+    v = c
+    return u, v
 
 
 class World(object):
@@ -8,27 +17,49 @@ class World(object):
     def __init__(self):
         self.world_h = 100
         self.world_w = 100
-        self._agents = [ag.RangeAgent(0, 0, 5, 0, 0, 45, self.world_h, self.world_w) for range(2)]
+        self._agents = [RangeAgent(50, 50, 5, 0, 0, 0, 45, self.world_h, self.world_w, 3) for _ in range(2)]
         self._brains = gen.random_generation(2, (4, 10), (10, 10))
         self._bullets = []
 
         
-        def collect_inputs(self):
-            pass
-        
-        
-        def process_ai(self):
-            inputs = self.collect_inputs()
-            behav_vectors = nn.forward_prop(inputs, self._brains)
-            for i, agent in enumerate(self._agents):
-                agent.choose_actions(behav_vectors[i])
-        
-        def update_physics(self, agent_actions, dt):
-            for bullet in self._bullets:
-                bullet.update(dt, self._agents)
+    def calc_rays(self):
+        rays = []
+        for agent in self._agents:
+            ns_vec = np.array([np.cos(agent.theta), np.sin(agent.theta)])
+            ew_vec = np.array([orthonormal_vector(agent.theta)])
+            breakpoint()
                 
-            for i, agent in enumerate(self._agents):
-                pass
+    def collect_inputs(self):
+        # Heading
+        
+        # FOV angle
+        
+        # N ray
+        # S ray
+        # E ray
+        # W ray
+        
+        # Enemy present
+        # Bullet present
+        self.calc_rays()
+            
+    def process_ai(self):
+        inputs = self.collect_inputs()
+        behav_vectors = nn.forward_prop(inputs, self._brains)
+        phys_info = []
+        for i, agent in enumerate(self._agents):
+            lr, dfov, bullets = agent.choose_actions(behav_vectors[i])
+            phys_info.append((lr, dfov))
+            self._bullets.extend(bullets)
+                
+        return phys_info
+        
+    def update_physics(self, phys_info, dt):
+        for bullet in self._bullets:
+            bullet.update(dt, self._agents)
+                
+        for i, agent in enumerate(self._agents):
+            agent.update(*phys_info[i], dt, self._agents)
         
         
 class MainGame(object):
