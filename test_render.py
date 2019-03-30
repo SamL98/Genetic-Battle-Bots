196bb366fd71f4ca3d79605ac232ab4a45c3d08c
Sam Lerner <lerner98@gmail.com>
2019-03-30 17:13:19 -0400

    Added None check in bullet append

--- test_render.py
+++ test_render.py
@@ -12,7 +12,8 @@ a = MeleeAgent(75, 125, 10, 0, 0, 0, 10, 200, 200, 3, 35, 3)
 a.melee_r = 10
 a.m_theta = 75
 a.theta = 0
-a.fov = 50
+a.fov = 10
+going_up = True
 
 cv.namedWindow('canvas')
 
@@ -22,9 +23,17 @@ for _ in range(360):
     cv.imshow('canvas', canv)
 
     a.theta = a.theta + 1
-    a.fov = a.fov + 1
-    if a.fov >= 90:
+
+    if going_up:
+        a.fov = a.fov + 1
+        if a.fov >= 90:
+            a.fov = a.fov - 1
+            going_up = False
+    else:
         a.fov = a.fov - 1
+        if a.fov <= 0:
+            a.fov = a.fov + 1
+            going_up = True:
 
     k = cv.waitKey(20)
     if k == 27:
