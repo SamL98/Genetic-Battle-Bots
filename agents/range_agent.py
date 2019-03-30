7cc26565bd30c14ddbc2a0b2cbdef1129be6d370
John Honaker <honaker215@gmail.com>
2019-03-30 17:41:03 -0400

    Add shot magnitude.

--- agents/range_agent.py
+++ agents/range_agent.py
@@ -4,26 +4,31 @@ from .bullet import Bullet
 from time import time
 
 class RangeAgent(Agent):
-	def __init__(self, x, y, r, theta, vx, vy, fov, w, h, num_lives):
-		super().__init__(x, y, r, theta, vx, vy, fov, w, h, num_lives)
-		self.agent_type = AgentType.Range
-		self.cooldown_time = 0.75
-		self.last_attack_time = 0
+        def __init__(self, x, y, r, theta, vx, vy, fov, w, h, num_lives):
+                super().__init__(x, y, r, theta, vx, vy, fov, w, h, num_lives)
+                self.agent_type = AgentType.Range
+                self.cooldown_time = 0.75
+                self.last_attack_time = 0
+                
+                self.shot_magnitude = 10
 
-	def attack(self):
-		if time()-self.last_attack_time < self.cooldown_time:
-			return
+        def attack(self):
+                if time()-self.last_attack_time < self.cooldown_time:
+                        return
 
-		super().attack()
-		bullet = Bullet(self.circ.x, self.circ.y, self.circ.r, self.vx, self.vy, *self.world_dims, self) 
-		self.last_attack_time = time()
-		return bullet
+                super().attack()
+                bullet = Bullet(self.circ.x, self.circ.y, self.circ.r,
+                                self.shot_magnitude * self.vx,
+                                self.shot_magnitude * self.vy,
+                                *self.world_dims, self) 
+                self.last_attack_time = time()
+                return bullet
 
-	def choose_action(self, behavior_vec):
-		lr, dfov, shd_attack = super().choose_action(behavior_vec)
-		bullets = []
-		if shd_attack:
-			bullet = self.attack()
-			if bullet:
-				bullets.append(bullet)
-		return lr, dfov, bullets
+        def choose_action(self, behavior_vec):
+                lr, dfov, shd_attack = super().choose_action(behavior_vec)
+                bullets = []
+                if shd_attack:
+                        bullet = self.attack()
+                        if bullet:
+                                bullets.append(bullet)
+                return lr, dfov, bullets
